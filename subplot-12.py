e1bfc58a6b2f84a24fd3e5a5c6182958e11c0578
Pravin <pravin22jd@gmail.com>
2018-12-24 10:04:00 +0530

    another way for subplots

--- subplot-12.py
+++ subplot-12.py
@@ -1,5 +1,5 @@
 ################################################################################################
-#	name:	subplot-11.py
+#	name:	subplot-12.py
 #	desc:	Subplot using matplotlib
 #	date:	2018-12-24
 #	Author:	conquistadorjd
@@ -10,8 +10,9 @@ import numpy as np
 print('*** Program Started ***')
 
 t= np.arange(1,17,1) 
+t2= np.arange(1,49,1) 
 y1= np.random.randint(1, 16,size=16)
-y2= np.random.randint(1, 32,size=16)
+y2= np.random.randint(1, 32,size=48)
 y3= np.random.randint(1, 48,size=16)
 y4= np.random.randint(1, 64,size=16)
 y5= np.random.randint(1, 124,size=16)
@@ -20,17 +21,16 @@ ax1 = plt.subplot2grid((5,3), (0,0), colspan=3, rowspan=1)
 plt.plot(t, y1)
 
 ax2 = plt.subplot2grid((5,3), (1,0), colspan=2,rowspan=1)
-plt.plot(t, y2)
+plt.plot(t2, y2)
 
-
-ax3 = plt.subplot2grid((5,3), (1, 2), colspan=2,rowspan=2)
+ax3 = plt.subplot2grid((5,3), (1, 2), colspan=2,rowspan=2,sharex=ax1)
 plt.plot(t, y3)
 
 ax4 = plt.subplot2grid((5,3), (2, 0), colspan=2,rowspan=1)
-plt.plot(t, y3)
+plt.plot(t, y4)
 
 ax5 = plt.subplot2grid((5,3), (3, 0), colspan=3,rowspan=2,sharex=ax1)
-plt.plot(t*2, y3)
+plt.plot(t*2, y5)
 
 # ax6 = plt.subplot2grid((5,3), (4, 0), colspan=3,rowspan=1,sharex=ax1)
 # plt.plot(t, y3)
